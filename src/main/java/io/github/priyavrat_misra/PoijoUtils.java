8538f05fd7694489800abd8cc2c9fede2ccbd96e
Priyavrat Misra <priyavratmisra123@gmail.com>
2025-02-15 15:14:29 +0530

    add skeleton code

---
+++ src/main/java/io/github/priyavrat_misra/PoijoUtils.java
@@ -0,0 +1,9 @@
+package io.github.priyavrat_misra;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class PoijoUtils {
+  public static <T> Workbook toWorkbook(T object, Class<T> objectClass) {
+    return null;
+  }
+}
