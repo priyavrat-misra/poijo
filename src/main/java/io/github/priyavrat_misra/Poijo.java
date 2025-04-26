2fe35ba8427475fcd27297fc3916b7e9107b02bd
Priyavrat Misra <priyavratmisra123@gmail.com>
2025-04-26 19:51:37 +0530

    update javadoc

--- src/main/java/io/github/priyavrat_misra/Poijo.java
+++ src/main/java/io/github/priyavrat_misra/Poijo.java
@@ -1,17 +1,24 @@
 package io.github.priyavrat_misra;
 
+import io.github.priyavrat_misra.annotations.Column;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
-import org.apache.poi.ss.usermodel.CellPropertyType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** A façade with a fluent interface to the Apache POI APIs. */
+/**
+ * A façade with a fluent interface to the Apache POI APIs.
+ *
+ * @author Priyavrat Misra
+ */
 public class Poijo {
   public static final String SPACE = " ";
   public static final String EMPTY = "";
@@ -24,6 +31,12 @@ public class Poijo {
     this.workbook = workbook;
   }
 
+  /**
+   * The main entry point for using {@link Poijo} methods.
+   *
+   * @param workbook a {@link Workbook} on which operations are to be performed
+   * @return a newly created {@link Poijo} object for chaining
+   */
   public static Poijo using(Workbook workbook) {
     if (workbook == null) {
       logger.error("workbook is null");
@@ -32,8 +45,167 @@ public class Poijo {
     return new Poijo(workbook);
   }
 
+  /**
+   * Maps {@code object} to {@link Workbook} that was passed in {@link Poijo#using(Workbook)}.
+   *
+   * <p>Structurally the {@code object} is considered a workbook, and the {@link
+   * java.util.Collection} data members are considered as sheets, where each element is a row. Refer
+   * example below for more clarity.
+   *
+   * <p>Supports the following types:
+   *
+   * <ul>
+   *   <li>types that are supported by {@link org.apache.poi.ss.usermodel.Cell}{@code ::setCell}
+   *       ({@link String}, {@link Integer}, {@code int}, {@link Double}, {@code double}, {@link
+   *       Boolean}, {@code boolean}, {@link RichTextString}, {@link Date}, {@link LocalDate},
+   *       {@link LocalDateTime} or {@link Calendar})
+   *   <li>{@link java.util.Collection}
+   *   <li>nested POJOs (should be annotated with {@link Column#nested()} set to {@code true})
+   * </ul>
+   *
+   * <p>Nested objects and lists are handled in a recursive manner, resulting in a flattened
+   * representation, suitable for the two-dimensional view of a spreadsheet. The title for a nested
+   * field is the path of field names to it from the base class delimited by {@link
+   * io.github.priyavrat_misra.annotations.Workbook#delimiter()}.
+   *
+   * <p>Note: Only {@code public} fields are considered because in order to access other kind of
+   * variables, the accessibility level has to be altered via reflection, and altering or bypassing
+   * the accessibility of classes, methods, or fields through reflection violates the encapsulation
+   * principle.
+   *
+   * <p>Example:
+   *
+   * <p>Say there are two classes {@code Author} and {@code Book}, and a base class, {@code Library}
+   * (should be annotated with {@link io.github.priyavrat_misra.annotations.Workbook}).
+   *
+   * <pre><code>
+   * {@literal @}Workbook // to indicate this class will be used as a Workbook
+   * public class Library {
+   *     // custom sheet name can be provided using {@literal @}Sheet(name = "...")
+   *     public Set&lt;Book&gt; books;
+   * }
+   * </code></pre>
+   *
+   * <pre><code>
+   * {@literal @}Order({"title", "author", "publicationDate", "price"}) // column ordering
+   * public class Book {
+   *     public String title;
+   *
+   *     {@literal @}Column(nested = true) // to indicate nesting
+   *     public Author author;
+   *
+   *     {@literal @}Column(numberFormat = "[$$-409]#,##0;-[$$-409]#,##0") // custom number format
+   *     public double price;
+   *
+   *     {@literal @}Column(name = "Date of Publication", numberFormat = "dd/MM/yyyy") // custom title
+   *     public LocalDate publicationDate;
+   *
+   *     public Book(String title, Author author, double price, LocalDate publicationDate) {
+   *         this.title = title;
+   *         this.author = author;
+   *         this.price = price;
+   *         this.publicationDate = publicationDate;
+   *     }
+   * }
+   * </code></pre>
+   *
+   * <pre>{@code
+   * public class Author {
+   *     public String name;
+   *     public List<String> genres;
+   *
+   *     public Author(String name, List<String> genres) {
+   *         this.name = name;
+   *         this.genres = genres;
+   *     }
+   * }
+   * }</pre>
+   *
+   * <p>Now all it takes is something as simple as the following:
+   *
+   * <pre>{@code
+   * public class Main {
+   *     public static void main(String[] args) {
+   *         // prepare data
+   *         Library library = new Library();
+   *         library.books = Arrays.asList(
+   *           new Book(
+   *             "The Hobbit",
+   *             new Author("J.R.R. Tolkien", Arrays.asList("Fantasy", "Adventure")),
+   *             14.99,
+   *             LocalDate.parse("1937-09-21")),
+   *           new Book(
+   *             "Harry Potter and the Sorcerer's Stone",
+   *             new Author("J.K. Rowling", Arrays.asList("Fantasy", "Drama", "Young Adult")),
+   *             19.99,
+   *             LocalDate.parse("1997-06-26")));
+   *
+   *         try (Workbook workbook = new XSSFWorkbook();
+   *             OutputStream fileOut = Files.newOutputStream(Paths.get("workbook.xlsx"))) {
+   *           // this is where the magic happens!
+   *           Poijo.using(workbook)
+   *              .map(library)
+   *              .applyCellStyleProperties(Collections.singletonMap(CellPropertyType.ALIGNMENT, HorizontalAlignment.CENTER))
+   *              .write(fileOut);
+   *         } catch (IOException e) {
+   *           throw new RuntimeException(e);
+   *         }
+   *     }
+   * }
+   * }</pre>
+   *
+   * <p>The spreadsheet will look something like the following in a sheet named <i>"Books"</i>.
+   *
+   * <table style='text-align: center;'>
+   *   <tr>
+   *     <th>Title</th>
+   *     <th>Author Name</th>
+   *     <th>Author Genres 0</th>
+   *     <th>Author Genres 1</th>
+   *     <th>Author Genres 2</th>
+   *     <th>Date of Publication</th>
+   *     <th>Price</th>
+   *   </tr>
+   *   <tr>
+   *     <td>The Hobbit</td>
+   *     <td>J.R.R. Tolkien</td>
+   *     <td>Fantasy</td>
+   *     <td>Adventure</td>
+   *     <td></td>
+   *     <td>21/09/1937</td>
+   *     <td>$15</td>
+   *   </tr>
+   *   <tr>
+   *     <td>Harry Potter and the Sorcerer's Stone</td>
+   *     <td>J.K. Rowling</td>
+   *     <td>Fantasy</td>
+   *     <td>Drama</td>
+   *     <td>Young Adult</td>
+   *     <td>26/06/1997</td>
+   *     <td>$20</td>
+   *   </tr>
+   * </table>
+   *
+   * <p>For header and body specific styling, {@link #applyCellStylePropertiesToHeader} and {@link
+   * #applyCellStylePropertiesToBody} can be used respectively.
+   *
+   * @param object an object which is to be mapped to the {@link Workbook} passed in {@link
+   *     Poijo#using(Workbook)}
+   * @return {@code this} instance for chaining
+   * @param <T> type parameter for {@code object}
+   * @see io.github.priyavrat_misra.annotations.Workbook
+   * @see io.github.priyavrat_misra.annotations.Sheet
+   * @see io.github.priyavrat_misra.annotations.Column
+   * @see io.github.priyavrat_misra.annotations.Order
+   * @see Poijo#applyCellStylePropertiesToHeader(Map)
+   * @see Poijo#applyCellStylePropertiesToBody(Map)
+   * @see <a
+   *     href="https://wiki.sei.cmu.edu/confluence/display/java/SEC05-J.+Do+not+use+reflection+to+increase+accessibility+of+classes%2C+methods%2C+or+fields"
+   *     target="_blank">SEC05-J. Do not use reflection to increase accessibility of classes,
+   *     methods, or fields</a>
+   */
   public <T> Poijo map(T object) {
-    PoijoUtils.map(workbook, validate(object));
+    PojoMapper.map(workbook, validate(object));
     return this;
   }
 
@@ -59,6 +231,15 @@ public class Poijo {
     return object;
   }
 
+  /**
+   * Invokes {@link CellUtil#setCellStylePropertiesEnum(Cell, Map)} on each cell in the sheets of
+   * the workbook.
+   *
+   * @param styles the properties to be added to a cell style, as {{@link CellPropertyType}:
+   *     propertyValue}
+   * @return {@code this} instance for chaining
+   * @see CellUtil#setCellStylePropertiesEnum(Cell, Map)
+   */
   public Poijo applyCellStyleProperties(Map<CellPropertyType, Object> styles) {
     workbook.forEach(
         sheet ->
@@ -67,6 +248,15 @@ public class Poijo {
     return this;
   }
 
+  /**
+   * Invokes {@link CellUtil#setCellStylePropertiesEnum(Cell, Map)} on each cell of the first row in
+   * the sheets of the workbook.
+   *
+   * @param styles the properties to be added to a cell style, as {{@link CellPropertyType}:
+   *     propertyValue}
+   * @return {@code this} instance for chaining
+   * @see CellUtil#setCellStylePropertiesEnum(Cell, Map)
+   */
   public Poijo applyCellStylePropertiesToHeader(Map<CellPropertyType, Object> styles) {
     workbook.forEach(
         sheet ->
@@ -74,11 +264,20 @@ public class Poijo {
     return this;
   }
 
+  /**
+   * Invokes {@link CellUtil#setCellStylePropertiesEnum(Cell, Map)} on each cell except the first
+   * row in the sheets of the workbook.
+   *
+   * @param styles the properties to be added to a cell style, as {{@link CellPropertyType}:
+   *     propertyValue}
+   * @return {@code this} instance for chaining
+   * @see CellUtil#setCellStylePropertiesEnum(Cell, Map)
+   */
   public Poijo applyCellStylePropertiesToBody(Map<CellPropertyType, Object> styles) {
     workbook.forEach(
         sheet -> {
           Iterator<Row> rowIterator = sheet.rowIterator();
-          // skip header row
+          // skip first row
           if (rowIterator.hasNext()) {
             rowIterator.next();
           }
@@ -88,10 +287,21 @@ public class Poijo {
     return this;
   }
 
+  /**
+   * Write out {@link Workbook} passed in {@link Poijo#using(Workbook)} to an {@link OutputStream}.
+   *
+   * @param stream the java OutputStream you wish to write to
+   * @throws IOException if anything can't be written
+   */
   public void write(OutputStream stream) throws IOException {
     workbook.write(stream);
   }
 
+  /**
+   * Get the passed workbook instance.
+   *
+   * @return the {@link Workbook} instance passed in {@link Poijo#using(Workbook)}
+   */
   public Workbook getWorkbook() {
     return workbook;
   }
--- src/main/java/io/github/priyavrat_misra/PojoMapper.java
+++ src/main/java/io/github/priyavrat_misra/PojoMapper.java
@@ -16,184 +16,11 @@ import org.apache.poi.ss.util.WorkbookUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * An annotation and reflection based utility class serving as a façade for the Apache POI APIs.
- *
- * <p>This utility class provides a single entry point, {@link PoijoUtils#map(Workbook, Object)},
- * which maps nested POJOs to Apache POI {@link Workbook} objects, greatly reducing interaction with
- * the Apache POI APIs and off-by-one indexing errors. At its heart, it uses reflection and various
- * custom annotations ({@link io.github.priyavrat_misra.annotations.Workbook}, {@link
- * io.github.priyavrat_misra.annotations.Sheet}, {@link Column}, and {@link Order}) to achieve this
- * task.
- *
- * <p>Some key features of this class include:
- *
- * <ul>
- *   <li>mapping multiple collection of POJOs as separate {@link Sheet}s.
- *   <li>ability to add custom naming to spreadsheet columns and sheets.
- *   <li>add, remove or order columns and sheets with ease.
- *   <li>handling nested POJOs by flattening them.
- *   <li>support for adding custom number format codes.
- *   <li>supports all data types for which {@link Cell} has {@code setCellValue} declared.
- * </ul>
- *
- * <p>Usage example:
- *
- * <p>Say there are two classes {@code Author} and {@code Book}, and a base class, {@code DTO}
- * (should be annotated with {@link io.github.priyavrat_misra.annotations.Workbook}).
- *
- * <pre><code>
- * {@literal @}Workbook // to indicate this class will be used as a Workbook
- * public class DTO {
- *     // custom sheet name can be provided using {@literal @}Sheet(name = "...")
- *     public Set&lt;Book&gt; books;
- * }
- * </code></pre>
- *
- * <pre><code>
- * {@literal @}Order({"title", "author", "publicationDate", "price"}) // column ordering
- * public class Book {
- *     public String title;
- *
- *     {@literal @}Column(nested = true) // to indicate nesting
- *     public Author author;
- *
- *     {@literal @}Column(numberFormat = "[$$-409]#,##0;-[$$-409]#,##0") // custom number format
- *     public double price;
- *
- *     {@literal @}Column(name = "Date of Publication", numberFormat = "dd/MM/yyyy") // custom title
- *     public LocalDate publicationDate;
- *
- *     public Book(String title, Author author, double price, LocalDate publicationDate) {
- *         this.title = title;
- *         this.author = author;
- *         this.price = price;
- *         this.publicationDate = publicationDate;
- *     }
- * }
- * </code></pre>
- *
- * <pre>{@code
- * public class Author {
- *     public String name;
- *     public List<String> genres;
- *
- *     public Author(String name, List<String> genres) {
- *         this.name = name;
- *         this.genres = genres;
- *     }
- * }
- * }</pre>
- *
- * <p>Now all it takes is something as simple as the following:
- *
- * <pre>{@code
- * public class Main {
- *     public static void main(String[] args) {
- *         // prepare data
- *         DTO dto = new DTO();
- *         dto.books = Arrays.asList(
- *            new Book(
- *              "The Hobbit",
- *              new Author("J.R.R. Tolkien", Arrays.asList("Fantasy", "Adventure")),
- *              14.99,
- *              LocalDate.parse("1937-09-21")),
- *            new Book(
- *              "Harry Potter and the Sorcerer's Stone",
- *              new Author("J.K. Rowling", Arrays.asList("Fantasy", "Drama", "Young Adult")),
- *              19.99,
- *              LocalDate.parse("1997-06-26")));
- *
- *         // map data
- *         try (Workbook wb = new XSSFWorkbook();
- *             OutputStream fileOut = Files.newOutputStream(Paths.get("workbook.xlsx"))) {
- *           Poijo.map(wb, dto).write(fileOut);
- *         } catch (IOException e) {
- *           throw new RuntimeException(e);
- *         }
- *     }
- * }
- * }</pre>
- *
- * <p>The spreadsheet will look something like the following in a sheet named <i>"Books"</i>.
- *
- * <table>
- *   <tr>
- *     <th>Title</th>
- *     <th>Author Name</th>
- *     <th>Author Genres 0</th>
- *     <th>Author Genres 1</th>
- *     <th>Author Genres 2</th>
- *     <th>Date of Publication</th>
- *     <th>Price</th>
- *   </tr>
- *   <tr>
- *     <td>The Hobbit</td>
- *     <td>J.R.R. Tolkien</td>
- *     <td>Fantasy</td>
- *     <td>Adventure</td>
- *     <td></td>
- *     <td>21/09/1937</td>
- *     <td>$15</td>
- *   </tr>
- *   <tr>
- *     <td>Harry Potter and the Sorcerer's Stone</td>
- *     <td>J.K. Rowling</td>
- *     <td>Fantasy</td>
- *     <td>Drama</td>
- *     <td>Young Adult</td>
- *     <td>26/06/1997</td>
- *     <td>$20</td>
- *   </tr>
- * </table>
- *
- * <p>If some field is annotated with {@link Column#name()}, it is used as the column title.
- * Otherwise, the field's name is split by camel case, capitalized, joined using {@link
- * io.github.priyavrat_misra.annotations.Workbook#delimiter()} (default is {@link Poijo#SPACE}) and
- * used as the title. The same applies for {@link
- * io.github.priyavrat_misra.annotations.Sheet#name()} in the base class.
- *
- * <p>Note: Only {@code public} fields are considered because in order to access other kind of
- * variables, the accessibility level has to be altered via reflection, and altering or bypassing
- * the accessibility of classes, methods, or fields through reflection violates the encapsulation
- * principle.
- *
- * <p>In addition to the {@code public} criteria, the fields are further filtered based on their
- * type and properties. Only the fields which conform to the following are considered eligible for a
- * column.
- *
- * <ol>
- *   <li>The field's type is any of the types that are supported by {@code Cell::setCellValue}
- *       namely, {@link String}, {@link Integer}, {@code int}, {@link Double}, {@code double},
- *       {@link Boolean}, {@code boolean}, {@link RichTextString}, {@link Date}, {@link LocalDate},
- *       {@link LocalDateTime} or {@link Calendar}.
- *   <li>The field's type is a {@link Collection}.
- *   <li>The field is annotated with {@link Column#nested()} set to {@code true}.
- * </ol>
- *
- * <p>As for the base class, only fields which are {@code public} and are a {@link Collection} are
- * considered for a sheet.
- *
- * <p>Nested objects and lists are handled in a recursive manner, resulting in a flattened
- * representation, suitable for the two-dimensional view of a spreadsheet. The title for a nested
- * field is the path of field names to it from the base class delimited by {@link
- * io.github.priyavrat_misra.annotations.Workbook#delimiter()}.
- *
- * @see <a
- *     href="https://wiki.sei.cmu.edu/confluence/display/java/SEC05-J.+Do+not+use+reflection+to+increase+accessibility+of+classes%2C+methods%2C+or+fields"
- *     target="_blank">SEC05-J. Do not use reflection to increase accessibility of classes, methods,
- *     or fields</a>
- * @see io.github.priyavrat_misra.annotations.Workbook
- * @see io.github.priyavrat_misra.annotations.Sheet
- * @see io.github.priyavrat_misra.annotations.Column
- * @see io.github.priyavrat_misra.annotations.Order
- * @author Priyavrat Misra
- */
-class PoijoUtils {
+class PojoMapper {
 
-  private static final Logger logger = LoggerFactory.getLogger(PoijoUtils.class);
+  private static final Logger logger = LoggerFactory.getLogger(PojoMapper.class);
 
-  private PoijoUtils() {}
+  private PojoMapper() {}
 
   /**
    * Maps {@code object} to {@code workbook}.
@@ -201,15 +28,13 @@ class PoijoUtils {
    * @param workbook a non-null {@link Workbook} object annotated with {@link
    *     io.github.priyavrat_misra.annotations.Workbook}
    * @param object a non-null object to be mapped to {@code workbook}
-   * @return {@code workbook} with the {@code object mapped}
    * @param <T> type parameter for {@code object}
    */
-  static <T> Workbook map(Workbook workbook, T object) {
-    final long startTime = System.currentTimeMillis();
+  static <T> void map(Workbook workbook, T object) {
     logger.info("map start");
-    populateWorkbook(Objects.requireNonNull(workbook), object);
+    final long startTime = System.currentTimeMillis();
+    populateWorkbook(workbook, object);
     logger.info("map complete, time taken: {} ms", System.currentTimeMillis() - startTime);
-    return workbook;
   }
 
   /** Gets the fields eligible for sheets, for each creates a sheet and populates the data. */
@@ -489,7 +314,7 @@ class PoijoUtils {
    *
    * @param rowClass class of a sheet's element
    * @return list of eligible (and possibly ordered) sheet names as string
-   * @see PoijoUtils#getEligibleSheetFieldNames(Class)
+   * @see PojoMapper#getEligibleSheetFieldNames(Class)
    */
   private static List<String> getEligibleColumnNames(Class<?> rowClass) {
     final List<String> eligibleColumnNames =
@@ -523,7 +348,7 @@ class PoijoUtils {
 
   /**
    * {@link NoSuchFieldException} is ignored because it will never arise due to {@link
-   * PoijoUtils#getEligibleColumnNames(Class)} which only returns accessible fields.
+   * PojoMapper#getEligibleColumnNames(Class)} which only returns accessible fields.
    */
   private static Field getField(Class<?> clazz, String fieldName) {
     try {
@@ -536,7 +361,7 @@ class PoijoUtils {
 
   /**
    * {@link IllegalAccessException} is ignored because it will never arise due to {@link
-   * PoijoUtils#getEligibleColumnNames(Class)} which only returns accessible fields.
+   * PojoMapper#getEligibleColumnNames(Class)} which only returns accessible fields.
    */
   private static Object getFieldValue(Field field, Object obj) {
     try {
